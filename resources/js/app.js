660de9e47c1d8cc81151cb99f8b346c50fe8af1c
Rafał Majkowski <majkipl@gmail.com>
2022-06-06 13:08:26 +0200

    modify css + js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,14 @@
+// import './bootstrap';
+//
+// import '../sass/app.scss'
+
 import './bootstrap';
 
-import '../sass/app.scss'
+// TU IMPORTOWAĆ ZEWNĘTRZNE BIBLIOTEKI
+// import 'jquery-ui/dist/jquery-ui';
+
+import '../sass/app.scss';
+
+// import './modernizr-custom';
+
+import './starter';
